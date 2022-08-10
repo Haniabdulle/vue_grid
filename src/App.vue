635757b0e93966be01fc915c793dfa4c5d29fb6d
hani abdulle <haniamira24@gmail.com>
2022-08-10 16:21:00 -0600

    Complete

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- app div is a parent div -->
+  <div id="app">
+    <div id="box">
+      <twitter-data></twitter-data>
+    </div>
+  </div>
+</template>
+
+<script>
+// twitter data component will get imported from src/components/twitterdata
+import TwitterData from "@/components/TwitterData.vue";
+export default {
+  name: "App",
+  components: {
+    TwitterData,
+  },
+};
+</script>
+
+<style scoped>
+#box{
+  display: grid;
+  grid-auto-flow: row;
+}
+</style>
---
+++ src/components/TwitterData.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+   <button id="grid">Toggle </button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    // Mount will add Event listener //
+  mounted() {
+    this.show_tweet();
+    let grid = document.getElementById(`grid`);
+    grid.addEventListener(`click`, this.column_list);
+  },
+
+  methods: {
+    // Displays data from the data  //
+    show_tweet() {
+      let box = document.getElementById(`box`);
+      for (let i = 0; i < this[`data`].length; i++) {
+        box[`innerHTML`] += `<div class="content_item">
+            <h2> ${this[`data`][i][`user`]} </h2>
+            <p>${this[`data`][i][`date`]}</p>
+            <h2>${this[`data`][i][`tweet`]}</h2>
+            </div>`;
+      }
+    },
+    // Function will check for style property grid and auto flow and will convert them //
+    column_list() {
+      let box = document.getElementById(`box`);
+     if(box[`style`][`gridAutoFlow`] === `column`){
+        box[`style`][`gridAutoFlow`] =`row`;
+     } else {
+        box[`style`][`gridAutoFlow`] =`column`;
+     }
+    },
+  },
+// All the date for tweets: tweets,user name and date tweet was posted //
+  data() {
+    return {
+      data: [
+        {
+          tweet: `Hello, have a nice day!`,
+          user: `Hani`,
+          date: `01-03-2002`,
+        },
+        {
+          tweet: `Ramadan Mubarak!!`,
+          user: `Siham`,
+          date: `05-06-2014`,
+        },
+        {
+          tweet: `Salaam!!`,
+          user: `Maya`,
+          date: `02-04-2013`,
+        },
+        {
+          tweet: `Eid Mubarak!!`,
+          user: `Hanad`,
+          date: `09-02-2011`,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
